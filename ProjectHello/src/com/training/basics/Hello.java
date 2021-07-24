0f4c885406a8e034b7dde164bacd1c12d90df9ca
patilswara11 <patilswara635@gmail.com>
2021-07-24 11:13:07 -0400

    my first commit

---
+++ ProjectHello/src/com/training/basics/Hello.java
@@ -0,0 +1,8 @@
+package com.training.basics;
+
+public class Hello {
+    public static void main(String args[]) {
+
+        System.out.println("Hello ...Welcome to Java");
+    }
+}
---
+++ ProjectHello/src/com/training/basics/Initials.java
@@ -0,0 +1,16 @@
+package com.training.basics;
+
+public class Initials{
+    public static void main(String[] args) {
+        System.out.println("*************" + " *************" + "  ************");
+        System.out.println("*            " + " *            " + "  *           *" );
+        System.out.println("*            " + " *            " + "  *           *" );
+        System.out.println("*            " + " *            " + "  *           *" );
+        System.out.println("*************" + " *************" + "  ************");
+        System.out.println("            *" + "             *" + "  *");
+        System.out.println("            *" + "             *" + "  *");
+        System.out.println("            *" + "             *" + "  *");
+        System.out.println("*************" + " *************" + "  *"    );
+
+    }
+}
